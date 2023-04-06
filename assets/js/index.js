58fd62a9fe30051aff9f0c073eaf69112307dd17
lyonbett <lyon.bett@student.moringaschool.com>
2023-04-06 15:07:19 +0300

    implementing full js code

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,17 +5,39 @@
       //a name
     //this item will be appended to the item container
     //implement getBeer
+    const getBeer = () => {
       //fetch data from PUNK API
+      fetch(' https://api.punkapi.com/v2/beers?per_page=6')
       //then grap the response and convert to JSON
+      .then(resp => resp.json())
       //then render the beer data
-
+      .then(renderBeer)
+    }
       //implement renderBeer
+      const renderBeer = beerArray => {
         //for each beer from fetch
+        beerArray.forEach(beer => {
+            const {image_url, name} = beer;
           //create an image element
+          const img = document.createElement('img');
             //assign the image url to the image source
+            img.src = image_url;
+
             //create a heading element
+            const beerName = document.createElement('h3');
             //assign the beer name to the text
+            beerName.textContent = name;
+
             //create a container to hold the image and name
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.append(img,beerName);
             //append the container to item container
+            document.querySelector('.item-container').appendChild(card);
+        })
+      }
     //when the html page loads to the DOM
+    document.addEventListener('DOMContentLoaded', () => {
       //invoke getBeer
+      getBeer();
+})
