beb09a2a6db8dc6c09000c5213eb50aadd49c41c
lyonbett <lyon.bett@student.moringaschool.com>
2023-04-06 13:08:22 +0300

    pseudocode for js code

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+//As a user, when i first open up the application,i want to see a list of items.
+  //each item
+    //Will go into a container and will contan:
+      //an image
+      //a name
+    //this item will be appended to the item container
+    //implement getBeer
+      //fetch data from PUNK API
+      //then grap the response and convert to JSON
+      //then render the beer data
+
+      //implement renderBeer
+        //for each beer from fetch
+          //create an image element
+            //assign the image url to the image source
+            //create a heading element
+            //assign the beer name to the text
+            //create a container to hold the image and name
+            //append the container to item container
+    //when the html page loads to the DOM
+      //invoke getBeer
